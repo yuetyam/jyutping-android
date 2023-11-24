1e84ca242f28a5dc17301107d3b7f3cc7b3aa244
Bing ZHEUNG <bing@zheung.org>
2023-11-24 14:49:19 +0800

    Add more contents to the ExpressionsScreen

--- app/src/main/java/org/jyutping/jyutping/ui/cantonese/ExpressionsScreen.kt
+++ app/src/main/java/org/jyutping/jyutping/ui/cantonese/ExpressionsScreen.kt
@@ -59,10 +59,59 @@ fun ExpressionsScreen() {
                                         LabelEntry(type = LabelType.CHECKED, text = "單數：佢"),
                                         LabelEntry(type = LabelType.CHECKED, text = "複數：佢哋（佢等）"),
                                         LabelEntry(type = LabelType.INFO, text = "毋分性別、人、物，一律用佢。"),
-                                        LabelEntry(type = LabelType.INFO, text = "佢 亦作 渠、⿰亻渠")
+                                        LabelEntry(type = LabelType.INFO, text = "佢 亦作 渠、𠍲{⿰亻渠}")
                                 )
                         )
                 }
+                item {
+                        DifferentView(
+                                heading = "區分「係」以及「喺」",
+                                lines = listOf(
+                                        "係 hai6：謂語，義同「是」。",
+                                        "喺 hai2：表方位、時間，義同「在」。",
+                                        "例：我係曹阿瞞。",
+                                        "例：我喺天后站落車。"
+                                )
+                        )
+                }
+                item {
+                        DifferentView(
+                                heading = "區分「諗」以及「冧」",
+                                lines = listOf(
+                                        "諗 nam2：想、思考、覺得。",
+                                        "冧 lam3：表示倒塌、倒下。",
+                                        "例：我諗緊今晚食咩。",
+                                        "例：佢畀人㨃冧咗。"
+                                )
+                        )
+                }
+                item {
+                        DifferentView(
+                                heading = "區分「咁」以及「噉」",
+                                lines = listOf(
+                                        "咁 gam3，音同「禁」。",
+                                        "噉 gam2，音同「錦」。",
+                                        "例：我生得咁靚仔。",
+                                        "例：噉又未必。"
+                                )
+                        )
+                }
+        }
+}
+
+@Composable
+private fun DifferentView(heading: String, lines: List<String>) {
+        Column(
+                modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxWidth()
+                        .clip(shape = RoundedCornerShape(size = 8.dp))
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+                Text(text = heading, fontWeight = FontWeight.SemiBold)
+                lines.onEach { Text(text = it) }
         }
 }
 
