f27b01b222406aff4a604cb245d4fb410b732cb6
Bing ZHEUNG <bing@zheung.org>
2024-04-18 15:25:27 +0800

    Improve JyutpingScreen

--- app/src/main/java/org/jyutping/jyutping/ui/romanization/JyutpingScreen.kt
+++ app/src/main/java/org/jyutping/jyutping/ui/romanization/JyutpingScreen.kt
@@ -6,31 +6,44 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FormatListBulleted
+import androidx.compose.material.icons.automirrored.outlined.FormatListBulleted
 import androidx.compose.material.icons.outlined.Link
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import org.jyutping.jyutping.R
 import org.jyutping.jyutping.Screen
 import org.jyutping.jyutping.ui.common.NavigationLabel
+import org.jyutping.jyutping.ui.common.SearchField
 import org.jyutping.jyutping.ui.common.WebLinkLabel
 
 @Composable
 fun JyutpingScreen(navController: NavHostController) {
+        val textState = remember { mutableStateOf(TextFieldValue(text = "")) }
         LazyColumn(
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
         ) {
                 item {
                         Column {
-                                NavigationLabel(icon = Icons.Outlined.FormatListBulleted, text = stringResource(id = R.string.jyutping_label_initials)) {
+                                SearchField(state = textState, verticalPadding = 8.dp)
+                        }
+                }
+                item {
+                        Column(
+                                modifier = Modifier.padding(vertical = 16.dp),
+                                verticalArrangement = Arrangement.spacedBy(0.dp)
+                        ) {
+                                NavigationLabel(icon = Icons.AutoMirrored.Outlined.FormatListBulleted, text = stringResource(id = R.string.jyutping_label_initials)) {
                                         navController.navigate(route = Screen.JyutpingInitials.route)
                                 }
-                                NavigationLabel(icon = Icons.Outlined.FormatListBulleted, text = stringResource(id = R.string.jyutping_label_finals)) {
+                                NavigationLabel(icon = Icons.AutoMirrored.Outlined.FormatListBulleted, text = stringResource(id = R.string.jyutping_label_finals)) {
                                         navController.navigate(route = Screen.JyutpingFinals.route)
                                 }
                                 NavigationLabel(icon = Icons.Outlined.Notifications, text = stringResource(id = R.string.jyutping_label_tones)) {
@@ -40,18 +53,19 @@ fun JyutpingScreen(navController: NavHostController) {
                 }
                 item {
                         Column(
-                                modifier = Modifier.padding(vertical = 32.dp),
+                                modifier = Modifier.padding(vertical = 16.dp),
                                 verticalArrangement = Arrangement.spacedBy(8.dp)
                         ) {
                                 WebLinkLabel(icon = Icons.Outlined.Search, text = "粵音資料集叢", uri = "https://jyut.net")
                                 WebLinkLabel(icon = Icons.Outlined.Search, text = "粵典", uri = "https://words.hk")
                                 WebLinkLabel(icon = Icons.Outlined.Search, text = "粵語審音配詞字庫", uri = "https://humanum.arts.cuhk.edu.hk/Lexis/lexi-can")
                                 WebLinkLabel(icon = Icons.Outlined.Search, text = "泛粵典", uri = "https://www.jyutdict.org")
+                                WebLinkLabel(icon = Icons.Outlined.Search, text = "羊羊粵語", uri = "https://shyyp.net/hant")
                         }
                 }
                 item {
                         Column(
-                                modifier = Modifier.padding(vertical = 8.dp),
+                                modifier = Modifier.padding(vertical = 16.dp),
                                 verticalArrangement = Arrangement.spacedBy(8.dp)
                         ) {
                                 WebLinkLabel(icon = Icons.Outlined.Link, text = "粵拼 Jyutping", uri = "https://jyutping.org")
