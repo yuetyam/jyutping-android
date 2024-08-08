c8b9cfdd1fc7c9680c271426c66a936285e6f734
Bing ZHEUNG <bing@zheung.org>
2024-08-08 16:40:04 +0800

    Bump to 0.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
                 applicationId = "org.jyutping.jyutping"
                 minSdk = 29
                 targetSdk = 34
-                versionCode = 10
-                versionName = "0.10.0"
+                versionCode = 11
+                versionName = "0.11.0"
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
                 vectorDrawables.useSupportLibrary = true
         }
