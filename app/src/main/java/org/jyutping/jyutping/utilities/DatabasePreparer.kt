c8dab9d0b626c1db1bcc4efd852bda0d3ebf908e
Bing ZHEUNG <bing@zheung.org>
2024-09-08 09:38:15 +0800

    Bump to 0.17.0

--- app/src/main/java/org/jyutping/jyutping/utilities/DatabasePreparer.kt
+++ app/src/main/java/org/jyutping/jyutping/utilities/DatabasePreparer.kt
@@ -23,6 +23,8 @@ object DatabasePreparer {
                 "appdb-v0.14.0-tmp.sqlite3-journal",
                 "appdb-v0.15.0-tmp.sqlite3",
                 "appdb-v0.15.0-tmp.sqlite3-journal",
+                "appdb-v0.16.0-tmp.sqlite3",
+                "appdb-v0.16.0-tmp.sqlite3-journal",
         )
         val databaseName: String = run {
                 val version = BuildConfig.VERSION_NAME
