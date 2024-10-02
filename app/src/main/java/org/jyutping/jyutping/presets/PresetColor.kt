1f6d755ca074d8a4dca2881456c8d661446dc9bb
Bing ZHEUNG <bing@zheung.org>
2024-10-02 15:31:50 +0800

    Improve keyboard color schemes

--- app/src/main/java/org/jyutping/jyutping/presets/PresetColor.kt
+++ app/src/main/java/org/jyutping/jyutping/presets/PresetColor.kt
@@ -3,6 +3,27 @@ package org.jyutping.jyutping.presets
 import androidx.compose.ui.graphics.Color
 
 object PresetColor {
+        val keyLight               : Color = Color.White
+        val keyLightEmphatic       : Color = Color(0xFFBBBBBB)
+        val keyboardLightBackground: Color = Color(0xFFDDDDDD)
+
+        val keyDark                : Color = Color(0xFF666666)
+        val keyDarkEmphatic        : Color = Color(0xFF444444)
+        val keyboardDarkBackground : Color = Color(0xFF222222)
+}
+
+/*
+object AltPresetColor {
+        val keyLight               : Color = Color.White
+        val keyLightEmphatic       : Color = Color(0xFFB8BCC3)
+        val keyboardLightBackground: Color = Color(0xFFDDDFE3)
+
+        val keyDark                : Color = Color(0xFF666666)
+        val keyDarkEmphatic        : Color = Color(0xFF444444)
+        val keyboardDarkBackground : Color = Color(0xFF222222)
+}
+
+object IOSPresetColor {
         val keyboardLightBackground: Color = Color(0xFFD0D3D9)
         val keyboardDarkBackground : Color = Color(0xFF333333)
 
@@ -14,3 +35,4 @@ object PresetColor {
         val keyDarkOpacity         : Color = Color(0xFF686868)
         val keyDarkEmphaticOpacity : Color = Color(0xFF464646)
 }
+*/
