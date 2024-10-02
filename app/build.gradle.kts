e4f6ac9eb0670d6afad8bf4a09e89e09160089cc
Bing ZHEUNG <bing@zheung.org>
2024-10-02 13:16:58 +0800

    Upgrade gradle packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,30 +41,32 @@ android {
                 compose = true
                 buildConfig = true
         }
+        /*
         composeOptions {
-                kotlinCompilerExtensionVersion = "1.5.13"
+                kotlinCompilerExtensionVersion = "1.5.14"
         }
+        */
         packaging {
                 resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
 }
 
-val composeVersion = "1.6.8"
+val composeVersion = "1.7.2"
 dependencies {
-        implementation("androidx.activity:activity-compose:1.9.1")
-        implementation("androidx.activity:activity-ktx:1.9.1")
+        implementation("androidx.activity:activity-compose:1.9.2")
+        implementation("androidx.activity:activity-ktx:1.9.2")
         implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-        implementation("androidx.compose.material3:material3:1.2.1")
+        implementation("androidx.compose.material3:material3:1.3.0")
         implementation("androidx.compose.ui:ui:$composeVersion")
         implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
         implementation("androidx.core:core-ktx:1.13.1")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-        implementation("androidx.lifecycle:lifecycle-service:2.8.4")
-        implementation("androidx.navigation:navigation-compose:2.7.7")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+        implementation("androidx.lifecycle:lifecycle-service:2.8.6")
+        implementation("androidx.navigation:navigation-compose:2.8.1")
         implementation("com.louiscad.splitties:splitties-systemservices:3.0.0")
         implementation("com.louiscad.splitties:splitties-views:3.0.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.1")
         androidTestImplementation("androidx.test.ext:junit:1.2.1")
         androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
         androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
