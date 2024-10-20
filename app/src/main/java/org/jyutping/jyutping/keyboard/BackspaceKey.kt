a68a2b51998f894825339c18faf99092570e5f9e
Bing ZHEUNG <bing@zheung.org>
2024-10-21 02:39:01 +0800

    Improve BackspaceKey gesture handling

--- app/src/main/java/org/jyutping/jyutping/keyboard/BackspaceKey.kt
+++ app/src/main/java/org/jyutping/jyutping/keyboard/BackspaceKey.kt
@@ -1,6 +1,5 @@
 package org.jyutping.jyutping.keyboard
 
-import android.annotation.SuppressLint
 import android.view.HapticFeedbackConstants
 import android.view.SoundEffectConstants
 import androidx.compose.foundation.background
@@ -14,121 +13,113 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jyutping.jyutping.JyutpingInputMethodService
 import org.jyutping.jyutping.R
 import org.jyutping.jyutping.presets.PresetColor
 
-@SuppressLint("ReturnFromAwaitPointerEventScope")
 @Composable
 fun BackspaceKey(modifier: Modifier) {
-        var isPressed by remember { mutableStateOf(false) }
-        var isLongPressed by remember { mutableStateOf(false) }
-        var taskJob: Job? by remember { mutableStateOf(null) }
-        val scope = rememberCoroutineScope()
         val view = LocalView.current
         val context = LocalContext.current as JyutpingInputMethodService
         val isDarkMode = remember { context.isDarkMode }
-        var offsetX by remember { mutableFloatStateOf(0f) }
+        var isPressing by remember { mutableStateOf(false) }
+        var isLongPressing by remember { mutableStateOf(false) }
+        var longPressJob: Job? by remember { mutableStateOf(null) }
+        val longPressCoroutineScope = rememberCoroutineScope()
+        var isDraggable by remember { mutableStateOf(true) }
+        var isDragging by remember { mutableStateOf(false) }
         Box(
                 modifier = modifier
                         .pointerInput(Unit) {
-                                coroutineScope {
-                                        launch {
-                                                awaitPointerEventScope {
-                                                        while (true) {
-                                                                val event = awaitPointerEvent()
-                                                                when (event.type) {
-                                                                        PointerEventType.Press -> {
-                                                                                if (isPressed.not()) {
-                                                                                        isPressed = true
-                                                                                }
-                                                                        }
-                                                                        PointerEventType.Release -> {
-                                                                                if (isPressed) {
-                                                                                        isPressed = false
-                                                                                }
-                                                                        }
-                                                                }
+                                detectTapGestures(
+                                        onLongPress = {
+                                                isLongPressing = true
+                                                longPressJob = longPressCoroutineScope.launch {
+                                                        while (isLongPressing) {
+                                                                view.playSoundEffect(SoundEffectConstants.CLICK)
+                                                                view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+                                                                context.backspace()
+                                                                delay(100)
                                                         }
                                                 }
+                                        },
+                                        onPress = {
+                                                isPressing = true
+                                                tryAwaitRelease()
+                                                isPressing = false
+                                                isLongPressing = false
+                                                longPressJob?.cancel()
+                                        },
+                                        onTap = {
+                                                view.playSoundEffect(SoundEffectConstants.CLICK)
+                                                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+                                                context.backspace()
                                         }
-                                        launch {
-                                                detectTapGestures(
-                                                        onLongPress = {
-                                                                isLongPressed = true
-                                                                taskJob = scope.launch {
-                                                                        while (isLongPressed) {
-                                                                                view.playSoundEffect(SoundEffectConstants.CLICK)
-                                                                                view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
-                                                                                context.backspace()
-                                                                                delay(100)
-                                                                        }
-                                                                }
-                                                        },
-                                                        onPress = {
-                                                                tryAwaitRelease()
-                                                                isLongPressed = false
-                                                                taskJob?.cancel()
-                                                        },
-                                                        onTap = {
-                                                                view.playSoundEffect(SoundEffectConstants.CLICK)
-                                                                view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                                                                context.backspace()
-                                                        }
-                                                )
-                                        }
-                                        launch {
-                                                detectHorizontalDragGestures { change, dragAmount ->
-                                                        change.consume()
-                                                        offsetX += dragAmount
-                                                        if (offsetX < -44f) {
+                                )
+                        }
+                        .pointerInput(Unit) {
+                                detectHorizontalDragGestures(
+                                        onDragStart = {
+                                                isDraggable = true
+                                                isDragging = true
+                                        },
+                                        onDragEnd = {
+                                                isDraggable = true
+                                                isDragging = false
+                                        },
+                                        onDragCancel = {
+                                                isDraggable = true
+                                                isDragging = false
+                                        },
+                                        onHorizontalDrag = { change, dragAmount ->
+                                                change.consume()
+                                                if (isDraggable) {
+                                                        val offsetX = change.position.x - change.previousPosition.x
+                                                        if (offsetX < -20f) {
                                                                 view.playSoundEffect(SoundEffectConstants.CLICK)
                                                                 view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_RELEASE)
                                                                 context.clearBuffer()
+                                                                isDraggable = false
                                                         }
                                                 }
                                         }
-                                }
+                                )
                         }
                         .fillMaxSize(),
                 contentAlignment = Alignment.Center
         ) {
-                Box (
+                Box(
                         modifier = modifier
                                 .padding(horizontal = 3.dp, vertical = 6.dp)
-                                .clip(RoundedCornerShape(6.dp))
                                 .background(
-                                        if (isDarkMode.value) {
-                                                if (isPressed) PresetColor.keyDark else PresetColor.keyDarkEmphatic
+                                        color = if (isDarkMode.value) {
+                                                if (isPressing || isDragging) PresetColor.keyDark else PresetColor.keyDarkEmphatic
                                         } else {
-                                                if (isPressed) PresetColor.keyLight else PresetColor.keyLightEmphatic
-                                        }
+                                                if (isPressing || isDragging) PresetColor.keyLight else PresetColor.keyLightEmphatic
+                                        },
+                                        shape = RoundedCornerShape(6.dp)
                                 )
                                 .fillMaxSize(),
                         contentAlignment = Alignment.Center
                 ) {
                         Icon(
-                                imageVector = ImageVector.vectorResource(id = if (isPressed) R.drawable.key_backspacing else R.drawable.key_backspace),
+                                imageVector = ImageVector.vectorResource(id = if (isPressing) R.drawable.key_backspacing else R.drawable.key_backspace),
                                 contentDescription = null,
                                 modifier = Modifier.size(22.dp),
                                 tint = if (isDarkMode.value) Color.White else Color.Black
