d1ce0cb049a2cc27ea1d304bf1ff2a44191127b0
Bing ZHEUNG <bing@zheung.org>
2024-11-02 13:50:38 +0800

    Upgrade gradle packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,17 +51,17 @@ android {
         }
 }
 
-val composeVersion = "1.7.4"
+val composeVersion = "1.7.5"
 dependencies {
         implementation("androidx.activity:activity-compose:1.9.3")
         implementation("androidx.activity:activity-ktx:1.9.3")
         implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-        implementation("androidx.compose.material3:material3:1.3.0")
+        implementation("androidx.compose.material3:material3:1.3.1")
         implementation("androidx.compose.ui:ui:$composeVersion")
         implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-        implementation("androidx.core:core-ktx:1.13.1")
-        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-        implementation("androidx.lifecycle:lifecycle-service:2.8.6")
+        implementation("androidx.core:core-ktx:1.15.0")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+        implementation("androidx.lifecycle:lifecycle-service:2.8.7")
         implementation("androidx.navigation:navigation-compose:2.8.3")
         implementation("com.louiscad.splitties:splitties-systemservices:3.0.0")
         implementation("com.louiscad.splitties:splitties-views:3.0.0")
