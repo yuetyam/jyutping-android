04544d47b8b5a0240c0f12c4a6375ffb2a820133
Bing ZHEUNG <bing@zheung.org>
2026-06-22 16:10:53 +0800

    (preparing) Remove fineTun()

--- preparing/src/main/kotlin/Main.kt
+++ preparing/src/main/kotlin/Main.kt
@@ -1,8 +1,6 @@
 package org.jyutping.preparing
 
 import java.io.File
-import java.sql.DriverManager
-import kotlin.use
 
 fun main() {
         val dbPath = "../app/src/main/assets/appdb.sqlite3"
@@ -18,21 +16,4 @@ fun main() {
         val url = "jdbc:sqlite:$dbPath"
         AppDataPreparer.prepare(url)
         KeyboardDataPreparer.prepare(url)
-        fineTun(url)
-}
-
-private fun fineTun(url: String) {
-        val commands: List<String> = listOf(
-                "PRAGMA journal_mode = DELETE;",
-                "VACUUM;",
-                "ANALYZE;",
-        )
-        DriverManager.getConnection(url).use { connection ->
-                connection.createStatement().use { statement ->
-                        commands.forEach { command ->
-                                statement.execute(command)
-                        }
-                }
-        }
-        println("Fine-tuned database.")
 }
